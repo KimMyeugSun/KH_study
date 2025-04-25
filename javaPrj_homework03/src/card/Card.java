dafb50624e7d987ff8f11653a5e4c037fbb55c0c
KimMyeugSun <156988475+KimMyeugSun@users.noreply.github.com>
2025-04-25 19:11:22 +0900

    update hw3

--- javaPrj_homework03/src/card/Card.java
+++ javaPrj_homework03/src/card/Card.java
@@ -8,28 +8,17 @@ public class Card {
 	public Card(String name, String num) {
 		this.Pattern = name;
 		this.cardNum = num;
-	} 
+	}
 	public Card() {
 		
 	}	
 	String[] patterns = {"diamond", "spade", "heart", "club"};
 	String[] cardNums = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "k"};
-	int score = 0;
+	int score;
 	String Pattern;
 	String cardNum;
-	public int cardScore() {
-		int score = 0;
-		for(int i = 0; i < cardNums.length; i++) {
-			if(cardNums[i].equals("K") || cardNums[i].equals("Q")|| cardNums[i].equals("J")) {
-				score = 10;
-				break;
-			}else {
-				score = i;
-				break;
-			}
-		}
-		return score;
-	}
+	
+	
 	
 	
 }
--- javaPrj_homework03/src/card/CardManager.java
+++ javaPrj_homework03/src/card/CardManager.java
@@ -1,11 +1,15 @@
 package card;
 
 public class CardManager {
-// 1회 게임 시 모든 카드를 세팅하고 관리하기 위한 클래스
+/*
+ *  1회 게임 시 모든 카드를 세팅하고 관리하기 위한 클래스
+ *  각 카드 객체 배열에 무늬와, 숫자가 담겨있음
+ */
+	
 	Card[] cardArr = new Card[56];
 	Card card = new Card();
 	
-	public void setCard() {
+	public void setCardName() {
 		int index = 0;
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 14; j++) {
@@ -14,16 +18,28 @@ public class CardManager {
 			}
 		}
 	}
-	
-	// 카드가 맞게 입력이 되었는지 확인하기 위한 임시 메서드
-	public void namePrint() {
-		for(int i = 0; i < cardArr.length; i++) {
-			System.out.println(cardArr[i]);
+	public int cardScore() {
+		for(int i = 0; i < card.cardNums.length; i++) {
+			if(card.cardNums[i].equals("K") || card.cardNums[i].equals("Q")|| card.cardNums[i].equals("J")) {
+				card.score = 10;
+			}else {
+				card.score = i;
+			}
+		}
+		return card.score;
+	}
+	public void setCardScore() {
+		for(int i = 0; i < 56; i++) {
+			if(cardArr[i].cardNum)
 		}
 	}
-	
 
-	
-
+	public void scorePrint() {
+		for(int i = 0; i < cardArr.length; i++) { 
+			System.out.print(cardArr[i].Pattern + " ");
+			System.out.print(cardArr[i].cardNum + " ");
+			System.out.print(cardArr[i].score + "\n");
+		}
+	}
 
 }
